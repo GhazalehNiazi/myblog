ed2dc61a974248820608ee228f29f707ab7acae4
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-09-07 12:44:41 +0430

    initial commit

---
+++ src/components/HeroAboutMe.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function HeroAboutMe() {
+  return (
+    <div className=" p-4 text-slate-200">
+      <h1 className="text-3xl text-pink-500 font-medium pt-4">سلام, من غزاله‌ام! </h1>
+      <p className="text-xl font-medium" >
+        <br></br>
+        به صفحه‌ی شخصی من خوش اومدین.
+        <br></br>
+        <br></br>
+        فلان فلان فلا فلان فلان فلا فلان فلان فلا فلان فلان فلا فلان فلان فلا
+        فلان فلان فلا فلان فلان فلا من ریکت دولوپر هستم که به فلان فلان فلان
+        <br></br>
+        <br></br>
+        فلان فلان فلا فلان فلان فلا فلان فلان
+        <a className="text-pink-400 underline"> فلا فلان فلان </a>
+         فلا فلان فلان فلا فلان فلان فلا فلان فلان فلا فلان فلان فلا فلان فلان  
+        فلا فلان فلان فلا فلان فلان فلا فلان فلان فلا
+        <a className="text-pink-400 underline"> فلان </a> فلان فلا فلان فلان فلا
+      </p>
+    </div>
+  );
+}
+
+export default HeroAboutMe;
