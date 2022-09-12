786a5eec4e03fa2b8074650de822f19d8320691e
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-09-12 20:55:16 +0430

    article page is responsive

--- src/components/Articles.tsx
+++ src/components/Articles.tsx
@@ -2,8 +2,8 @@ import Article from "./Article";
 
 function Articles() {
   return (
-    <div className=" bg-neutral-800 ">
-      <div className="bg-neutral-800">
+    <div className=" bg-neutral-800 lg:max-h-min lg:mx-12  lg:grow lg:justify-self-center lg:self-start lg:bg-transparent lg:m-3 ">
+      <div className="bg-neutral-800 lg:p-4">
         <Article
           date="۵ اسفند ۱۴۰۰"
           title="چرا اینگونه و آنگونه و چطوری که اینطوری میشه و توضیحات و کلمه و کلمه"
