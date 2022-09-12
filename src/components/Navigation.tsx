3933ee29ddaacc87b6223aa84960289d4a545121
GhazalehNiazi <ghazaleniazisabet02@gmail.com>
2022-09-12 22:39:02 +0430

    change navLinks margin

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,6 +4,7 @@ const navItems: { name: string; link: string }[] = [
   { name: "درباره‌ی من", link: "/aboutme" },
   { name: "پروژه‌ها", link: "/projects" },
   { name: "مقاله‌ها", link: "/articles" },
+  { name: "خانه", link: "/" },
 ];
 
 function Navigation({
@@ -15,7 +16,7 @@ function Navigation({
 }) {
   return (
     <div
-      className={`bg-neutral-800 flex justify-between justify-items-center item-center content-center  py-5 text-slate-300 border-b border-neutral-700 px-4 `}
+      className={`bg-neutral-800 flex justify-between justify-items-center item-center content-center  py-5 text-slate-300 border-b border-neutral-700 px-10`}
     >
       <nav className=" flex justify-between text-m w-full px-3">
         <Link to="/" className="font-['Poppins'] tracking-widest mr-4 ">
@@ -29,7 +30,7 @@ function Navigation({
 
         <div className="hidden md:flex md:align-start px-2">
           {navItems.map((item) => (
-            <button key={item.link} className="mr-10">
+            <button key={item.link} className="mr-6">
               <NavLink
                 to={item.link}
                 className={({ isActive }) =>
